5a90e7eb02be9dcdef8601b31c6d7ccd1eeb66a1
DSnoad <danialsnoad@hotmail.com>
2022-12-29 16:55:34 +0000

    Add block content to duplicate code

--- run.py
+++ run.py
@@ -10,7 +10,22 @@ def index():
     return render_template("index.html")
 
 
-if __name__ == "__name__":
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
+
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
